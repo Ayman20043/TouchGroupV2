a24b079a303b3ddf3cdfcb6dbd6241b8581f230c
ayman20043 <ayman20043@yahoo.com>
2016-08-20 22:39:24 +0200

    Add project files.

---
+++ TouchGroupV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TouchGroupV2.MvcApplication" Language="C#" %>
---
+++ TouchGroupV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TouchGroupV2.Startup))]
+namespace TouchGroupV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
